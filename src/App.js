8b0dc84b51f83581d705eca1375b31406afd792f
”sovsianykov” <swebproger@gmail.com>
2021-02-05 12:40:16 +0200

    Revert "night 3"
    This reverts commit 2212e171

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { useSelector}  from 'react-redux';
+import { useSelector, useDispatch }  from 'react-redux';
 
 import {Header} from "./scss/components";
 import {Home,Cart} from "./pages";
 import {Route} from "react-router-dom";
+import {fetchPizzas } from './redux/actions/pizzas';
 import store  from './redux/store'
 
 function App() {
-    // const { items } = useSelector(({pizzas}) => {
-    //     return {
-    //         items: pizzas.items,
-    //
-    //     }
-    // })
+    const dispatch = useDispatch();
+    const { items } = useSelector(({pizzas}) => {
+        return {
+            items: pizzas.items,
+
+        }
+    })
 
 
+    React.useEffect(() => {
+        console.log(dispatch(fetchPizzas()))
 
+    },[]);
 
     return (
         <div className="wrapper">
